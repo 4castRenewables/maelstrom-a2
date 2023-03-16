64f4dca25af077dfa28843ef6cdea79f5e234c8a
Kristian Ehlert <kristian.ehlert@4-cast.de>
2023-03-16 13:53:30 +0100

    add timestamps to logging

--- scripts/finetune_bert.py
+++ scripts/finetune_bert.py
@@ -13,7 +13,7 @@ from a2.training import benchmarks as timer
 
 # from deep500.utils import timer_torch as timer
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(format="%(asctime)s %(levelname)-8s %(message)s", level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S")
 
 
 def main(args):
