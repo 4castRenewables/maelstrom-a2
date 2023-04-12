b4d2e8601cc0adf5c2a238ad91beed512380c82e
Kristian Ehlert <kristian.ehlert@4-cast.de>
2023-04-12 13:42:31 +0200

    renamed scripts to be more consistent

--- cluster/benchmarks2023/run_benchmark.sh
+++ cluster/benchmarks2023/run_benchmark.sh
@@ -3,4 +3,4 @@
 . /p/home/jusers/ehlert1/juwels/env.sh
 conda init
 conda activate a2-benchmarking
-python ../scripts/finetune_bert.py --run_folder test/ --data_filename tweets_2017_01_era5_normed_filtered_predicted_simpledeberta.nc
+python ../scripts/finetune_deberta.py --run_folder test/ --data_filename tweets_2017_01_era5_normed_filtered_predicted_simpledeberta.nc
