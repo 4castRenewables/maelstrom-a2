91407e463bb7e1f5f645d8c4c6db91c9e12c47a6
Kristian Ehlert <kristian.ehlert@4-cast.de>
2023-09-21 16:39:27 +0200

    rename epochs to fix bug

--- src/a2/utils/argparse.py
+++ src/a2/utils/argparse.py
@@ -308,7 +308,7 @@ def hyperparameter_basic(parser):
 def hyperparameter(parser):
     hyperparameter_basic(parser)
     # arg name should match name of hyper parameter used in `model_configs.py`!
-    parser.add_argument("--number_epochs", type=int, default=1, help="Numer of epochs to train.")
+    parser.add_argument("--epochs", type=int, default=1, help="Numer of epochs to train.")
     parser.add_argument("--batch_size", type=int, default=32, help="Number of samples per mini-batch.")
     parser.add_argument("--learning_rate", type=float, default=3e-05, help="Learning rate to train model.")
     parser.add_argument("--weight_decay", type=float, default=0, help="Weight decay rate to train model.")
