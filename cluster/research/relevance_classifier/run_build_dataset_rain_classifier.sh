13c6d38e980f115f1c93562cdf440ed3132af6d8
Kristian Ehlert <kristian.ehlert@4-cast.de>
2023-09-21 17:21:25 +0200

    update bash bug

--- cluster/research/relevance_classifier/run_build_dataset_rain_classifier.sh
+++ cluster/research/relevance_classifier/run_build_dataset_rain_classifier.sh
@@ -2,7 +2,7 @@ MODEL_NAME="electra_base"
 python ../../../scripts/relevance_classifier/build_dataset_rain_classifier.py \
     --filename_dataset_to_split /p/project/deepacf/maelstrom/ehlert1/data/dataset_split/2017-2020//relevance_prediction//RainingClassifierDataset2017_2020_tweets_rain_sun_vocab_emojis_locations_bba_Tp_era5_no_bots_normalized_filtered_weather_stations_fix_predicted_simpledeberta_radar_relevant_predicted.nc \
     --output_dir /p/project/deepacf/maelstrom/ehlert1/data/dataset_split/ \
-    --task_name 2017-2020_{MODEL_NAME} \
+    --task_name 2017-2020_${MODEL_NAME} \
     --test_size 0.2 \
     --validation_size 0.2 \
     --key_stratify raining \
