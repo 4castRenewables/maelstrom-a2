6198faaffe12563de27697c72113bd756dbd1061
Kristian Ehlert <kristian.ehlert@4-cast.de>
2024-02-02 17:25:25 +0100

    docker run if image doesnt exist

--- scripts/finetune_deberta/mlflow_projects/deberta_rain_classifier/run_docker_e4.sh
+++ scripts/finetune_deberta/mlflow_projects/deberta_rain_classifier/run_docker_e4.sh
@@ -1,1 +1,4 @@
-# docker run --name ap2_maelstrom -t -d --gpus all --ipc=host -e HOME=$HOME -e USER=$USER --mount "type=bind,source=${jube_benchmark_home}/../../scripts/finetune_deberta/mlflow_projects/deberta_rain_classifier/,target=/scripts" --mount "type=bind,source=${base_path_dataset},target=${base_path_dataset}" kristian4cast/ml:a2-cuda
+echo $image_exists
+if [ -z "$image_exists" ]; then
+    docker run --name ap2_maelstrom -t -d --gpus all --ipc=host -e HOME=$HOME -e USER=$USER --mount "type=bind,source=${jube_benchmark_home}/../../scripts/finetune_deberta/mlflow_projects/deberta_rain_classifier/,target=/scripts" --mount "type=bind,source=${base_path_dataset},target=${base_path_dataset}" kristian4cast/ml:a2-cuda
+fi
