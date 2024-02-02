7ba6504ee5987e8a23c1f6dcf02522cbc17e48bd
Kristian Ehlert <kristian.ehlert@4-cast.de>
2024-02-02 17:27:49 +0100

    fix docker ps chec

--- scripts/finetune_deberta/mlflow_projects/deberta_rain_classifier/run_docker_e4.sh
+++ scripts/finetune_deberta/mlflow_projects/deberta_rain_classifier/run_docker_e4.sh
@@ -1,3 +1,4 @@
+image_exists=$(docker ps | grep ap2_maelstrom)
 echo $image_exists
 if [ -z "$image_exists" ]; then
     docker run --name ap2_maelstrom -t -d --gpus all --ipc=host -e HOME=$HOME -e USER=$USER --mount "type=bind,source=${jube_benchmark_home}/../../scripts/finetune_deberta/mlflow_projects/deberta_rain_classifier/,target=/scripts" --mount "type=bind,source=${base_path_dataset},target=${base_path_dataset}" kristian4cast/ml:a2-cuda
