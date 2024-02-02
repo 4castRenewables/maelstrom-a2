e80c42ca8f0e60050d5f14eb3e4a983150681170
Kristian Ehlert <kristian.ehlert@4-cast.de>
2024-02-02 16:42:02 +0100

    convert docker usage from id to nam,e

--- scripts/finetune_deberta/mlflow_projects/deberta_rain_classifier/run_docker_e4.sh
+++ scripts/finetune_deberta/mlflow_projects/deberta_rain_classifier/run_docker_e4.sh
@@ -1,2 +1,1 @@
-docker_image_id=$(docker run -t -d --gpus all --ipc=host -e HOME=$HOME -e USER=$USER --mount "type=bind,source=${jube_benchmark_home}/../../scripts/finetune_deberta/mlflow_projects/deberta_rain_classifier/,target=/scripts" --mount "type=bind,source=${base_path_dataset},target=${base_path_dataset} nvcr.io/nvidia/pytorch:24.01-py3")
-export docker_image_id=${docker_image_id}
+docker run --name a2-cuda -t -d --gpus all --ipc=host -e HOME=$HOME -e USER=$USER --mount "type=bind,source=${jube_benchmark_home}/../../scripts/finetune_deberta/mlflow_projects/deberta_rain_classifier/,target=/scripts" --mount "type=bind,source=${base_path_dataset},target=${base_path_dataset} nvcr.io/nvidia/pytorch:24.01-py3"
