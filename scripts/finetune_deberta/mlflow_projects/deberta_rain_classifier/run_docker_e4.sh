392d5ebb01d43f6a5a09f3522c3914463b20afdc
Kristian Ehlert <kristian.ehlert@4-cast.de>
2024-02-05 09:49:24 +0100

    fix model path

--- scripts/finetune_deberta/mlflow_projects/deberta_rain_classifier/run_docker_e4.sh
+++ scripts/finetune_deberta/mlflow_projects/deberta_rain_classifier/run_docker_e4.sh
@@ -7,5 +7,6 @@ echo $image_exists
 if [ -z "$image_exists" ]; then
     docker run --name ap2_maelstrom -t -d --gpus all --ipc=host -e HOME=$HOME -e USER=$USER \
         --mount "type=bind,source=${jube_benchmark_home}/../../scripts/finetune_deberta/mlflow_projects/deberta_rain_classifier/,target=/scripts" \
-        --mount "type=bind,source=${base_path_dataset},target=${base_path_dataset}" kristian4cast/ml:a2-cuda
+        --mount "type=bind,source=${base_path_dataset},target=${base_path_dataset}" kristian4cast/ml:a2-cuda \
+        --mount "type=bind,source=${model_base_path},target=${model_base_path}" kristian4cast/ml:a2-cuda
 fi
