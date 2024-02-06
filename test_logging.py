800155289f0109ccfcfe52052caa6f2a25d46fa5
Kristian Ehlert <kristian.ehlert@4-cast.de>
2024-02-06 09:03:17 +0100

    test logging

---
+++ test_logging.py
@@ -0,0 +1,16 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename="py_log.log",
+    filemode="w",
+    format="%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s",
+)
+logger = logging.getLogger(__name__)
+logging.info("this is from logging")
+
+logger.debug("A DEBUG Message")
+logger.info("An INFO")
+logger.warning("A WARNING")
+logger.error("An ERROR")
+logger.critical("A message of CRITICAL severity")
